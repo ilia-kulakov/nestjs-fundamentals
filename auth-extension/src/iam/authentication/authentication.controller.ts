3e51590a9c21e7721e962bc63923a269cabae933
Ilia Kulakov <Ilia_Kulakov@epam.com>
2025-05-24 13:30:30 +0200

    auth-extension: Save JWT as Body

--- auth-extension/src/iam/authentication/authentication.controller.ts
+++ auth-extension/src/iam/authentication/authentication.controller.ts
@@ -1,15 +1,7 @@
-import {
-  Body,
-  Controller,
-  HttpCode,
-  HttpStatus,
-  Post,
-  Res,
-} from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { SignUpDto } from './dto/sign-up.dto/sign-up.dto';
 import { SignInDto } from './dto/sign-in.dto/sign-in.dto';
-import { Response } from 'express';
 
 @Controller('authentication')
 export class AuthenticationController {
@@ -22,15 +14,7 @@ export class AuthenticationController {
 
   @HttpCode(HttpStatus.OK)
   @Post('sign-in')
-  async signIn(
-    @Res({ passthrough: true }) response: Response,
-    @Body() signInDto: SignInDto,
-  ) {
-    const accessToken = await this.authService.signIn(signInDto);
-    response.cookie('accessToken', accessToken, {
-      secure: true,
-      httpOnly: true,
-      sameSite: true,
-    });
+  signIn(@Body() signInDto: SignInDto) {
+    return this.authService.signIn(signInDto);
   }
 }
