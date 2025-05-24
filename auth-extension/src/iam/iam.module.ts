0455716a7c4dcbfc51b196173c61c42f1bc1255c
Ilia Kulakov <Ilia_Kulakov@epam.com>
2025-05-24 15:58:43 +0200

    auth-extension: Expose public routes

--- auth-extension/src/iam/iam.module.ts
+++ auth-extension/src/iam/iam.module.ts
@@ -10,6 +10,7 @@ import jwtConfig from './config/jwt.config';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { AccessTokenGuard } from './authentication/guards/access-token/access-token.guard';
+import { AuthenticationGuard } from './authentication/guards/authentication/authentication.guard';
 
 // Identity and access managment
 @Module({
@@ -25,8 +26,9 @@ import { AccessTokenGuard } from './authentication/guards/access-token/access-to
     },
     {
       provide: APP_GUARD,
-      useClass: AccessTokenGuard,
+      useClass: AuthenticationGuard,
     },
+    AccessTokenGuard,
     AuthenticationService,
   ],
   controllers: [AuthenticationController],
